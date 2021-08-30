dabaae2b66f73278c963ad73fe4e07785547f5aa
Steven Schronk <steven@schronk.com>
2021-08-30 14:04:48 -0500

    Added cross products and do products.

--- RayTracer/RayTracerTest.c
+++ RayTracer/RayTracerTest.c
@@ -51,6 +51,49 @@ struct tuple tupleNegate(struct tuple t) {
   return ret;
 }
 
+struct tuple tupleMultScalar(struct tuple t, float s) {
+  struct tuple ret = { t.x * s, t.y * s, t.z * s, t.w *s };
+  return ret;
+}
+
+struct tuple tupleDivScalar(struct tuple t, float s) {
+  struct tuple ret = { t.x / s, t.y / s, t.z / s, t.w / s };
+  return ret;
+}
+
+float tupleMagVec(struct tuple t) {
+  float magx = pow(t.x, 2);
+  float magy = pow(t.y, 2);
+  float magz = pow(t.z, 2);
+  float mag = sqrtf( magx + magy + magz);
+  return mag;
+}
+
+struct tuple normVec(struct tuple t) {
+  float mag = tupleMagVec(t);
+  struct tuple ret = { t.x / mag, t.y / mag, t.z / mag};
+  return ret;
+}
+
+float dot(struct tuple t1, struct tuple t2) {
+  float prod1 = t1.x * t2.x;
+  float prod2 = t1.y * t2.y;
+  float prod3 = t1.z * t2.z;
+  float prod4 = t1.w * t2.w;
+  float dot = prod1 + prod2 + prod3 + prod4;
+  return dot;
+}
+
+struct tuple cross(struct tuple a, struct tuple b) {
+  float x = a.y * b.z - a.z * b.y;
+  float y = a.z * b.x - a.x * b.z;
+  float z = a.x * b.y - a.y * b.x;
+  struct tuple cross = createVector(x, y, z);
+  return cross;
+}
+
+/*-------------------------------------------------------------*/
+
 // 4 creates tuples with w=1
 void createPointTest() {
   struct tuple t = createPoint(4.0f, -4.0f, 3.0f);
@@ -149,6 +192,107 @@ void negatingTupleTest() {
   assert(equal(vec1.z, -3.0f));
 }
 
+// 8 Multiply tuple by a scalar
+void tupleMultScalarTest() {
+  struct tuple vec1 = { 1.0f, -2.0f, 3.0f, -4.0f };
+  float scalar = 3.5f;
+  vec1 = tupleMultScalar(vec1, scalar);
+  assert(equal(vec1.x,   3.5f));
+  assert(equal(vec1.y,  -7.0f));
+  assert(equal(vec1.z,  10.5f));
+  assert(equal(vec1.w, -14.0f));
+}
+
+// 8 Multiply tuple by a fraction
+void tupleMultScalarFractionTest() {
+  struct tuple vec1 = { 1.0f, -2.0f, 3.0f, -4.0f };
+  float scalar = 0.5f;
+  vec1 = tupleMultScalar(vec1, scalar);
+  assert(equal(vec1.x, 0.5f));
+  assert(equal(vec1.y, -1.0f));
+  assert(equal(vec1.z, 1.5f));
+  assert(equal(vec1.w, -2.0f));
+}
+
+// 8 Divide a tuple by a scalar
+void tupleDivScalarTest() {
+  struct tuple vec1 = { 1.0f, -2.0f, 3.0f, -4.0f };
+  float scalar = 2.0f;
+  vec1 = tupleDivScalar(vec1, scalar);
+  assert(equal(vec1.x, 0.5f));
+  assert(equal(vec1.y, -1.0f));
+  assert(equal(vec1.z, 1.5f));
+  assert(equal(vec1.w, -2.0f));
+}
+
+// 8 Computing the magnitude of vector(1, 0, 0)
+void tupleMagVecTest() {
+  struct tuple vec1 = createVector(1.0f, 0.0f, 0.0f);
+  float mag = tupleMagVec(vec1);
+  assert(equal(mag, 1.0f));
+
+  struct tuple vec2 = createVector(0.0f, 1.0f, 0.0f);
+  mag = tupleMagVec(vec2);
+  assert(equal(mag, 1.0f));
+
+  struct tuple vec3 = createVector(0.0f, 0.0f, 1.0f);
+  mag = tupleMagVec(vec3);
+  assert(equal(mag, 1.0f));
+
+  struct tuple vec4 = createVector(1.0f, 2.0f, 3.0f);
+  mag = tupleMagVec(vec4);
+  assert(equal(mag, sqrt(14.0f)));
+
+  struct tuple vec5 = createVector(-1.0f, -2.0f, -3.0f);
+  mag = tupleMagVec(vec5);
+  assert(equal(mag, sqrt(14.0f)));
+}
+
+// 10 Normalizing vector(4,0,0) gives (1,0,0)
+void normVecTest() {
+  struct tuple vec1 = createVector(4.0f, 0.0f, 0.0f);
+  struct tuple norm = normVec(vec1);
+  assert(equal(norm.x, 1.0f));
+  assert(equal(norm.y, 0.0f));
+  assert(equal(norm.z, 0.0f));
+
+  struct tuple vec2 = createVector(1.0f, 2.0f, 3.0f);
+  norm = normVec(vec2);
+  float ans1 = 1 / sqrt(14);
+  float ans2 = 2 / sqrt(14);
+  float ans3 = 3 / sqrt(14);
+  assert(equal(norm.x, ans1));
+  assert(equal(norm.y, ans2));
+  assert(equal(norm.z, ans3));
+
+  struct tuple vec3 = createVector(1.0f, 2.0f, 3.0f);
+  norm = normVec(vec3);
+  float mag = tupleMagVec(norm);
+  assert(equal(mag, 1.0f));
+}
+
+// 10 dot rpoduct of two tuples
+void dotTest() {
+  struct tuple vec1 = createVector(1.0f, 2.0f, 3.0f);
+  struct tuple vec2 = createVector(2.0f, 3.0f, 4.0f);
+  float dotProd = dot(vec1, vec2);
+  assert(equal(dotProd, 20.0f));
+}
+
+// 11 cross product of two vectors
+void crossTest() {
+  struct tuple vec1 = createVector(1.0f, 2.0f, 3.0f);
+  struct tuple vec2 = createVector(2.0f, 3.0f, 4.0f);
+  struct tuple cross1 = cross(vec1, vec2);
+  assert(equal(cross1.x, -1.0f));
+  assert(equal(cross1.y,  2.0f));
+  assert(equal(cross1.z, -1.0f));
+  struct tuple cross2 = cross(vec2, vec1);
+  assert(equal(cross2.x,  1.0f));
+  assert(equal(cross2.y, -2.0f));
+  assert(equal(cross2.z,  1.0f));
+}
+
 int main() {
   createPointTest();
   createPointTest();
@@ -159,5 +303,12 @@ int main() {
   subtractTwoVectorsTest();
   subtractVectorFromZeroVectorTest();
   negatingTupleTest();
+  tupleMultScalarTest();
+  tupleMultScalarFractionTest();
+  tupleDivScalarTest();
+  tupleMagVecTest();
+  normVecTest();
+  dotTest();
+  crossTest();
   return 0;
 }
