c845b9353d50af2fc08dba94b413968ac83aa3c1
Steven Schronk <steven@schronk.com>
2021-09-23 15:20:28 -0500

    Added batch file to convert from ppm to png for sharing online.

---
+++ RayTracer/convert_output_to_png.bat
@@ -0,0 +1,6 @@
+magick convert canvas.ppm canvas.png
+rem magick convert canvas.ppm canvas_ppm.bmp
+rem magick convert canvas.png canvas_bmp.bmp
+rem magick compare -verbose canvas.ppm canvas.png -compose src diff.txt
+echo Exit Code is %errorlevel%
+pause
